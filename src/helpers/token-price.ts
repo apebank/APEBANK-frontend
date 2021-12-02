0ef4730c994f343161ce15bb8d4368e61632b14d
mocpaul <>
2021-12-02 17:43:28 +0800

    use usdc instead of usdt

--- src/helpers/token-price.ts
+++ src/helpers/token-price.ts
@@ -3,10 +3,10 @@ import axios from "axios";
 const cache: { [key: string]: number } = {};
 
 export const loadTokenPrices = async () => {
-    const url = "https://api.coingecko.com/api/v3/simple/price?ids=tether,boba-network&vs_currencies=usd";
+    const url = "https://api.coingecko.com/api/v3/simple/price?ids=usd-coin,boba-network&vs_currencies=usd";
     const { data } = await axios.get(url);
 
-    cache["USDT"] = data["tether"].usd;
+    cache["USDT"] = data["usd-coin"].usd;
     cache["BOBA"] = data["boba-network"].usd;
 };
 
