43c2ec45f16c6f5d9dc0e021b306836b232426a5
mocpaul <>
2021-12-08 11:07:53 +0800

    fix auto stake issue

--- src/store/slices/stake-thunk.ts
+++ src/store/slices/stake-thunk.ts
@@ -93,7 +93,7 @@ export const changeStake = createAsyncThunk("stake/changeStake", async ({ action
     try {
         const gasPrice = await getGasPrice(provider);
         if (action === "stake") {
-            stakeTx = await stakingHelper.stake(ethers.utils.parseUnits(value, "gwei"), { gasPrice });
+            stakeTx = await stakingHelper.stake(ethers.utils.parseUnits(value, "gwei"), address, { gasPrice });
         } else {
             stakeTx = await staking.unstake(ethers.utils.parseUnits(value, "gwei"), true, { gasPrice });
         }
